7ad244691409672aa55fdf42db2061ab9915e6dc
Abhishek2122-star <abhisheklad963@gmail.com>
2025-12-07 00:28:48 +0530

    js is updated for fetch live Quotes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Load quotes from public API
+document.getElementById("newQuoteBtn").addEventListener("click", loadQuote);
+
 function loadQuote() {
-    fetch("https://api.quotable.io/random")
-        .then(response => response.json())
+    fetch("https://api.adviceslip.com/advice?" + Math.random())
+        .then(res => res.json())
         .then(data => {
-            document.getElementById("quote").textContent = data.content;
+            document.getElementById("quote").textContent = data.slip.advice;
         })
         .catch(() => {
-            document.getElementById("quote").textContent = "Failed to load quote.";
+            document.getElementById("quote").textContent = 
+                "Unable to load quote. Please try again.";
         });
 }
 
